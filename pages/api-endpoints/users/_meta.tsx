a10b7d14547d864317fa8ac0b7f18c5f6200806e
Yanay Zabary <yanay.tsabary@gmail.com>
2025-09-29 17:24:07 +0300

    Updated follow and coection api documentatio

--- pages/api-endpoints/users/_meta.tsx
+++ pages/api-endpoints/users/_meta.tsx
@@ -1,10 +1,9 @@
 export default {
   "fetch-user": "Fetch User",
   "fetch-user-by-foreign-id": "Fetch User by Foreign ID",
-  "fetch-followers-count": "Fetch Followers Count",
-  "fetch-following-count": "Fetch Following Count",
   "check-username-availability": "Check Username Availability",
   "fetch-user-suggestions": "Get User Suggestions",
   "update-user": "Update User",
-  follows: "follows",
+  follows: "Follow Operations",
+  connections: "Connection Operations",
 };
